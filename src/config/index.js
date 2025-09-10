471fba8ce021e5d5ea263b9470e4565e5465fec7
Qingyun Wang <qingyun322@gmail.com>
2025-09-09 23:26:14 -0700

    fix axios network issue

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
 import { BASE_URL, API_KEY } from '@env';
 
+console.log('BASE_URL before', BASE_URL);
+console.log('API_KEY before', API_KEY);
+
+
 export default {
   apiUrl: BASE_URL,
   apiKey: API_KEY,
